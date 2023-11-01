3652ca52eb0649ca417fcdf55c440d3bd090a09d
awaltherr <anton.walther@live.com>
2023-11-01 16:05:16 +0100

    Added CORS and imported it in index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
-const app = express(),
-  port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.send({ hejsan: "Svejsan" });
 });
 
+port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server redo på http://localhost:${port}`);
 });
